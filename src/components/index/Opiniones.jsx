2109e5755549f533355999c1b5c73f3059248c36
meeeww <thezasm03@gmail.com>
2024-01-17 18:43:25 +0100

    Opiniones empezado

--- src/components/index/Opiniones.jsx
+++ src/components/index/Opiniones.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line react/prop-types
 export default function Opiniones({ nombre, titulo, opinion, estrellas }) {
-  const renderStars = () => {
-    let stars = [];
-    for (let i = 1; i <= 5; i++) {
-      stars.push(<i className="fa-solid fa-star w-4 h-4"></i>);
-    }
-    console.log(stars);
-    return stars;
-  };
+  // const renderStars = () => {
+  //   let stars = [];
+  //   for (let i = 1; i <= 5; i++) {
+  //     stars.push(<i className="fa-solid fa-star w-4 h-4"></i>);
+  //   }
+  //   console.log(stars);
+  //   return stars;
+  // };
 
   return (
     <main className="h-screen flex flex-col justify-center items-center">
