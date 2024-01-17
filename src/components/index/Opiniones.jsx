a1c5ef43260c0d0adbbaf1ca8d41a4b0412f3acf
meeeww <thezasm03@gmail.com>
2024-01-17 18:43:18 +0100

    Opiniones empezado

--- src/components/index/Opiniones.jsx
+++ src/components/index/Opiniones.jsx
@@ -1,3 +1,19 @@
-export default function Opiniones() {
+// eslint-disable-next-line react/prop-types
+export default function Opiniones({ nombre, titulo, opinion, estrellas }) {
+  const renderStars = () => {
+    let stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(<i className="fa-solid fa-star w-4 h-4"></i>);
+    }
+    console.log(stars);
+    return stars;
+  };
 
+  return (
+    <main className="h-screen flex flex-col justify-center items-center">
+      <div className="w-[350px] h-[350px] rounded-md gradiente">
+        <img src="icons/starsolid.svg" alt="" className="filter brightness-110 contrast-100 saturate-150 hue-rotate-15" />
+      </div>
+    </main>
+  );
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,6 +3,7 @@ import Layout from '../layout/Layout.jsx';
 import Titulo from '../components/index/Titulo.jsx';
 import ComoFunciona from '../components/index/ComoFunciona.jsx';
 import PorqueEscogernos from "../components/index/PorqueEscogernos.jsx";
+import Opiniones from '../components/index/Opiniones.jsx';
 
 export default function Index() {
   return (
@@ -15,6 +16,9 @@ export default function Index() {
       </section>
       <section className="border-t mx-12 rounded-none border-02">
         <PorqueEscogernos />
+      </section>
+      <section className="border-t mx-12 rounded-none border-02">
+        <Opiniones username={"Zas"} title={"Hey"} review={"yo"} rating={3} />
       </section>
     </Layout>
   );
