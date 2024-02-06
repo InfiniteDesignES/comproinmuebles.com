a87c71aea0dbfd49d560fd6bc21dd4441b935e05
Zas <thezasm03@gmail.com>
2024-02-06 17:47:28 +0100

    Movidos los archivos

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { AnimatePresence } from 'framer-motion';
-
 import Index from './pages/Index.jsx';
-import Index from './pages/Index.jsx'
-import InformacionVivienda from './components/InformacionVivienda.jsx'
-import TipoVivienda from './components/TipoVivienda.jsx'
-import RelacionExterior from './components/RelacionExterior.jsx'
-import EspaciosPrincipalesBoton from './components/EspaciosPrincipalesBoton.jsx'
 import Vender from './pages/Vender.jsx'
-import TipoVivienda from './components/venderahora/segundo/TipoVivienda.jsx'
-
-import InformacionVivienda from './pages/Vender/InformacionVivienda.jsx';
-import TipoVivienda from './pages/Vender/TipoVivienda.jsx';
 
 import './styles/Tailwind.css';
 import './styles/Main.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-=======
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import EspaciosPrincipales from './components/EspaciosPrincipales.jsx'
-import EspaciosComunes from './components/EspaciosComunes.jsx'
-import EstadoVivienda from './components/EstadoVivienda.jsx'
-import PorqueVender from './components/PorqueVender.jsx'
-import EnviarContacto from './components/EnviarContacto.jsx'
-import MetrosCuadrados from './components/MetrosCuadrados.jsx'
 
 const router = createBrowserRouter([
   {
@@ -38,7 +16,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/vender',
-    element: <InformacionVivienda />
+    element: <Vender />
   },
 ]);
 
--- src/pages/Vender.jsx
+++ src/pages/Vender.jsx
@@ -2,30 +2,43 @@ import { useState } from 'react';
 import Header from '../layout/Header';
 import InformacionVivienda from '../components/venderahora/primero/InformacionVivienda';
 import TipoVivienda from '../components/venderahora/segundo/TipoVivienda';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Vender() {
-  // 0 para InformacionVivienda, 1 para TipoVivienda
-  const [mostrarFormulario, setMostrarFormulario] = useState(0);
+  const [mostrarSiguiente, setMostrarSiguiente] = useState(false);
 
-  const handleCambiarFormulario = () => {
-    setMostrarFormulario(1);
-  };
-
-  const renderComponent = () => {
-    switch (mostrarFormulario) {
-      case 0:
-        return <InformacionVivienda setSiguiente={handleCambiarFormulario} />;
-      case 1:
-        return <TipoVivienda />;
-      default:
-        return null;
-    }
+  // Función para alternar los componentes
+  const toggleComponent = () => {
+    setMostrarSiguiente(!mostrarSiguiente);
   };
 
   return (
-    <main className="flex h-screen items-center justify-center bg-[url('images/Background2.svg')] bg-cover">
+    <main className="flex h-screen items-center justify-center overflow-hidden bg-[url('images/Background2.svg')] bg-cover">
       <Header />
-      {renderComponent()}
+      <AnimatePresence>
+        {!mostrarSiguiente ? (
+          <motion.div
+            key="informacionVivienda"
+            initial={{ x: '100vw' }}
+            animate={{ x: 0 }}
+            exit={{ x: 0 }}
+            transition={{ type: 'spring', stiffness: 40 }}
+          >
+            <InformacionVivienda onSubmit={toggleComponent} />
+            {/* <TipoVivienda onVolver={toggleComponent} /> */}
+          </motion.div>
+        ) : (
+          <motion.div
+            key="siguienteComponente"
+            initial={{ x: '100vw' }}
+            animate={{ x: 0 }}
+            exit={{ x: '-100vw' }}
+            transition={{ type: 'spring', stiffness: 40}}
+          >
+            <TipoVivienda onVolver={toggleComponent} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 }
